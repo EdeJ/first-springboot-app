715791e679a7ef0d5e1f3851756641356021b452
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-04 07:58:09 +0100

    deleted imports

--- src/main/java/com/emieldejong/firstproject/controller/ClientController.java
+++ src/main/java/com/emieldejong/firstproject/controller/ClientController.java
@@ -9,14 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
